ccbfe725f7049e457a150e6b47a0f80f87eaf9e2
Arman D <adrismir@gmail.com>
2024-10-04 19:59:37 -0700

    fix

--- api/tests/TestSimSwap.py
+++ api/tests/TestSimSwap.py
@@ -18,13 +18,3 @@ def TestSimSwap():
 
 if __name__ == "__main__":
 	TestSimSwap()
-
-# curl -X POST "https://pplx.azurewebsites.net/api/rapid/v0/numberVerification/verify" \
-#         -H "Authorization: Bearer bca7ba" \
-#         -H "Cache-Control: no-cache" \
-#         -H "accept: application/json" \
-#         -H "Content-Type: application/json" \
-#         -d '{
-#           "phoneNumber": "14372197463"
-#         }'
-# {"status":200,"method":"POST","endpoint":"numberVerification","api":"verify","accessToken":"Bearer bca7ba","phoneNumber":"14372197463","message":"numberVerification request successful","datetime":"2024-10-05T00:33:53.441Z","cell":{"phoneNumber":"14372197463","name":"team 2","message":"Hi","accessToken":"Bearer bca7ba"},"device":{},"area":{},"devicePhoneNumberVerified":true,"devicePhoneNumber":"14372197463"}
