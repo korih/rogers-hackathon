dcea2716b8d1a3bf8857aed0bb6ae022a27a883a
CihanBosnali <cihanbosnali@gmail.com>
2024-10-05 23:05:19 -0700

    Added rogers API calls, event logging, requirements

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,17 @@
-from flask import Flask, request, jsonify, Response
+from flask import Flask
+import requests
+import json
+import time
+
+ROGERS_URL = "https://pplx.azurewebsites.net/api/rapid/v0"
+
+
+def get_rogers_headers(access_token):
+    return {"Authorization": "Bearer " + access_token,
+            "Cache-Control": "no-cache",
+            "accept": "application/json",
+            "Content-Type": "application/json"}
+
 
 users_list = [
                {
@@ -16,10 +29,81 @@ verification_results = []
 
 
 def verify(phone_number):
-    # TODO: Do Rogers checks
+    logfile = open("eventlog.txt", "a")
+    # find user associated with user in our database
+    user = None
+    for u in users_list:
+        if u["phone_number"] == phone_number:
+            user = u
+
+    if user is None:
+        return
+
+    # Check that the number user accesses from is the phone number
+    # associated with the Rogers user
+    data = json.dumps({"phoneNumber": phone_number})
+    num_verify_res = requests.post(ROGERS_URL + "/numberVerification/verify",
+                                   data=data,
+                                   headers=get_rogers_headers(user["token"]))
+
+    logfile.write("\n***** NEW NUMBER VERIFY REQUEST - "
+                  + time.ctime() + " *****\n")
+    logfile.write(data)
+    logfile.write("\n----- NUMBER VERIFY RESPONSE -----\n")
+    logfile.write(num_verify_res.text)
+
+    if (not num_verify_res.json()["devicePhoneNumberVerified"]):
+        verification_results.append({"phone_number": phone_number,
+                                     "result": "numverify fail"})
+        logfile.close()
+        return
+
+    # Check that the SIM was not swapped in last 240 hours
+    sim_verify_res = requests.post(ROGERS_URL + "/simswap/check",
+                                   data=data,
+                                   headers=get_rogers_headers(user["token"]))
+
+    logfile.write("\n***** NEW SIM SWAP VERIFY REQUEST - "
+                  + time.ctime() + " *****\n")
+    logfile.write(data)
+    logfile.write("\n----- SIM SWAP VERIFY RESPONSE -----\n")
+    logfile.write(sim_verify_res.text)
+
+    if (sim_verify_res.json()["swapped"]):
+        verification_results.append({"phone_number": phone_number,
+                                     "result": "simswap fail"})
+        logfile.close()
+        return
+
+    data = json.dumps({"device": {"phoneNumber": phone_number},
+                       "area": {
+                           "type": "Circle",
+                           "location": {
+                               "latitude": user["latitude"],
+                               "longitude": user["longitude"]
+                           },
+                           "accuracy": user["accuracy"]
+                       }})
+
+    loc_verify_res = requests.post(ROGERS_URL + "/location-verification/verify",
+                                   data=data,
+                                   headers=get_rogers_headers(user["token"]))
+
+    logfile.write("\n***** NEW LOCATION VERIFY REQUEST - "
+                  + time.ctime() + " *****\n")
+    logfile.write(data)
+    logfile.write("\n----- LOCATION VERIFY RESPONSE -----\n")
+    logfile.write(sim_verify_res.text)
+
+    if (not loc_verify_res.json()["verificationResult"]):
+        verification_results.append({"phone_number": phone_number,
+                                     "result": "location fail"})
+        logfile.close()
+        return
+
     verification_results.append({"phone_number": phone_number,
                                  "result": "pass"})
-
+    logfile.close()
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
